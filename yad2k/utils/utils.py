a27572a79dc51d12687cb477dcd6014f9135ecf1
Jeel Ranpariya <69497902+jeelranpariya@users.noreply.github.com>
2022-07-08 11:59:03 +0530

    Update utils.py

--- yad2k/utils/utils.py
+++ yad2k/utils/utils.py
@@ -91,7 +91,7 @@ def draw_boxes(image, boxes, box_classes, class_names, scores=None):
     #font = ImageFont.load_default()
     #font='/content/drive/MyDrive/360_work/font/FiraMono-Medium.otf',
     font = ImageFont.truetype(
-        font = 'FiraMono-Medium.otf',
+        font = '/content/FiraMono-Medium.otf',
         size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
     thickness = (image.size[0] + image.size[1]) // 300
 
